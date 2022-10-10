4cea794cc2d7f90ab5ad38d7cb827bd7e773d4eb
rdani2005 <dricardosc15@gmail.com>
2022-10-10 16:01:26 -0600

    Starting a new web server app, that's gonna be used on an own server app

---
+++ web-server/target/debug/deps/web_server-5b7f1451613391bf.d
@@ -0,0 +1,5 @@
+/home/rdani2005/proyectos/rust/web-server/target/debug/deps/web_server-5b7f1451613391bf: src/main.rs
+
+/home/rdani2005/proyectos/rust/web-server/target/debug/deps/web_server-5b7f1451613391bf.d: src/main.rs
+
+src/main.rs:
---
+++ web-server/target/debug/web-server.d
@@ -0,0 +1,1 @@
+/home/rdani2005/proyectos/rust/web-server/target/debug/web-server: /home/rdani2005/proyectos/rust/web-server/src/main.rs
