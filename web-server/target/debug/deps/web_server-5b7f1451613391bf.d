3ecf03ae5e3dde5ad900c53ab104612dd9951222
rdani2005 <dricardosc15@gmail.com>
2022-10-15 14:44:58 -0600

    changes

--- web-server/target/debug/deps/web_server-5b7f1451613391bf.d
+++
@@ -1,5 +0,0 @@
-/home/rdani2005/proyectos/rust/web-server/target/debug/deps/web_server-5b7f1451613391bf: src/main.rs
-
-/home/rdani2005/proyectos/rust/web-server/target/debug/deps/web_server-5b7f1451613391bf.d: src/main.rs
-
-src/main.rs:
--- web-server/target/debug/web-server.d
+++
@@ -1,1 +0,0 @@
-/home/rdani2005/proyectos/rust/web-server/target/debug/web-server: /home/rdani2005/proyectos/rust/web-server/src/main.rs
